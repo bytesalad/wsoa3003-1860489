c4f1f0dea13ee9e9e30edacf189ef8acad9e96b8
bytesalad <matthewmarcobrown@gmail.com>
2020-05-20 01:24:24 +0200

    New base mechanics progress

---
+++ wsoa3003-exam-game/Assets/Scripts/Blue/blue_avatar_coupling.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class blue_avatar_coupling : MonoBehaviour
+{
+    [SerializeField] private float follow_speed;
+    private GameObject avatar;
+    private bool follow = false;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Avatar")
+        {
+            avatar = collision.gameObject;
+            follow = true;
+        }
+    }
+
+    private void Update()
+    {
+        if (follow)
+        {
+            gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2((avatar.transform.position.x - transform.position.x) * follow_speed,
+                (avatar.gameObject.transform.position.y - transform.position.y) * follow_speed);
+        }
+    }
+}
---
+++ wsoa3003-exam-game/Assets/Scripts/Goal/Goal_Trigger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Goal_Trigger : MonoBehaviour
+{
+    [SerializeField] private string next_scene_name;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Blue")
+        {
+            //SceneManager.LoadScene(next_scene_name);
+            Debug.Log("Level Finished");
+        }
+    }
+}
